1bb93c1b81d291b78150d3702a53559d02fcfe42
Max696 <maxferdiaz@gmail.com>
2018-02-21 18:32:29 -0600

    First

--- Lab1Ejemplo-master/EjemploLab1/Controllers/CVSController.cs
+++ Lab1Ejemplo-master/EjemploLab1/Controllers/CVSController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using EjemploLab1.DBContext;
+using System.IO;
+using EjemploLab1.DBContext;
+using EjemploLab1.Models;
 
 namespace EjemploLab1.Controllers
 {
@@ -14,6 +17,7 @@ namespace EjemploLab1.Controllers
         // GET: CVS
         public ActionResult Index()
         {
+            Session["Lista"] = Session["Lista"] ?? db.Personas;
             return View(db.Personas.ToList());
         }
 
@@ -31,13 +35,51 @@ namespace EjemploLab1.Controllers
 
         // POST: CVS/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(HttpPostedFileBase excelFile)
         {
             try
             {
-                // TODO: Add insert logic here
+                string Fpath = string.Empty;
+                if(excelFile!=null)
+                {
+                    string path= Server.MapPath("~/Files/");
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+                    Fpath = path + Path.GetFileName(excelFile.FileName);
+                    string extension = Path.GetExtension(excelFile.FileName);
+                    excelFile.SaveAs(Fpath);
+                    string csvData = System.IO.File.ReadAllText(Fpath);
+                    int count = 0;
+                    foreach (var row in csvData.Split('\n'))
 
-                return RedirectToAction("Index");
+                    {
+                        if( !string.IsNullOrEmpty(row))
+                        {
+                            if (count ==0)
+                            {
+                                count++;
+                            }
+                            else
+                            {
+                                db.Personas.Add(new Persona
+                                {
+                                    Club = row.Split(',')[0],
+                                    Apellido = row.Split(',')[1],
+                                    Nombre = row.Split(',')[2],
+                                    Posicion = row.Split(',')[3],
+                                    Salario = double.Parse(row.Split(',')[4]),
+                                    
+                                });
+                            }
+
+
+                        }
+                        
+                    }
+                }
+               return View(db.Personas);
             }
             catch
             {
